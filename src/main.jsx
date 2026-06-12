16a445369434a3f3e0300b9a60dff8f0ff54ca6c
KenNedy2785 <drken.holyhills@gmail.com>
2026-06-12 06:38:02 +0000

    Initial LegonMed platform launch

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LegonMed from './LegonMed_Platform.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LegonMed />
+  </React.StrictMode>
+)
